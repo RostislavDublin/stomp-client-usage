5675f536fa74ff3f5fcaed1fb0eac460f6ed5abe
RostislavDublin <crm_guru@mail.ru>
2019-09-04 20:09:14 +0300

    Just last improvement for avoiding broken connection dead wait

--- src/test/java/stomp/sonic20432/SendToWrongThenRightQueue.java
+++ src/test/java/stomp/sonic20432/SendToWrongThenRightQueue.java
@@ -17,22 +17,22 @@ public class SendToWrongThenRightQueue {
         //client.send("/queue/SampleQ11", "Message to the good queue");
 
         client.subscribe("/queue/SampleQ1", "sq1", "auto");
-        client.subscribe("/queue/SampleQ2", "sq2", "auto");
-        client.subscribe("/queue/SampleQ3", "sq3", "auto");
-
-        client.subscribe("/topic/T1", "st1", "auto");
-        client.subscribe("/topic/T2", "st2", "auto");
-        client.subscribe("/topic/T3", "st3", "auto");
+//        client.subscribe("/queue/SampleQ2", "sq2", "auto");
+//        client.subscribe("/queue/SampleQ3", "sq3", "auto");
+//
+//        client.subscribe("/topic/T1", "st1", "auto");
+//        client.subscribe("/topic/T2", "st2", "auto");
+//        client.subscribe("/topic/T3", "st3", "auto");
 
         client.send("/queue/SampleQ1", "Message to the queue SampleQ1");
-        client.send("/queue/SampleQ2", "Message to the queue SampleQ2");
-        client.send("/queue/SampleQ3", "Message to the queue SampleQ3");
-
-        Helper.sleep(5000);
-
-        client.send("/topic/T1", "Message to the topic T1");
-        client.send("/topic/T2", "Message to the topic T2");
-        client.send("/topic/T3", "Message to the topic T3");
+//        client.send("/queue/SampleQ2", "Message to the queue SampleQ2");
+//        client.send("/queue/SampleQ3", "Message to the queue SampleQ3");
+//
+//        Helper.sleep(5000);
+//
+//        client.send("/topic/T1", "Message to the topic T1");
+//        client.send("/topic/T2", "Message to the topic T2");
+//        client.send("/topic/T3", "Message to the topic T3");
 
         Helper.sleep(5000);
 
